4fda2e09e8233b784cb4d21313877bfc772914d1
Gary Wallis <ggw@unixservice.com>
2025-12-25 23:25:43 -0300

    Update BohleBots_BNO055.cpp
    PlatformIO compile error

--- src/BohleBots_BNO055.cpp
+++ src/BohleBots_BNO055.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "Arduino.h"
 #include "Wire.h"
-#include "EEPROM.h"
+//#include "EEPROM.h"
 #include "stdint.h"
 #include "BohleBots_BNO055.h"
 
